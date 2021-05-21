36fbbbaf5abf1560e7d0a1671a2fb0b2efe548e0
Atharva <atharvagta007@gmail.com>
2021-05-21 12:22:30 +0530

    initial colmmit

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import watchdog
 from watchdog.events  import FileSystemEventHandler
 from watchdog.observers import Observer
+import sys
+class ToWatch():
+    watchDirectory = "C:\Users\Atharva\AppData\Local\Packages\Microsoft.ScreenSketch_8wekyb3d8bbwe\TempState"#your directory, in this case its the temperory snip and sketch folder for me
+    
 

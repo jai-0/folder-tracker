2b24c262ed1371e8141aa5f33e3639d8799b3159
Atharva <atharvagta007@gmail.com>
2021-05-21 12:36:21 +0530

    initial commit

--- main.py
+++ main.py
@@ -21,4 +21,13 @@ class ToWatch:
             print("Observer Stopped")
   
         self.observer.join()
-        
+class Handler(FileSystemEventHandler):
+    @staticmethod
+    def on_any_change(event):
+        if event.is_directory:
+            return None
+  
+        elif event.event_type == 'created':
+            # Event is created, you can process it now
+            print("Watchdog received created event - % s." % event.src_path)
+            

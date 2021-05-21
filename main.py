735411e00866e17dc2cc39d034a8ec408519fc16
Atharva <atharvagta007@gmail.com>
2021-05-21 12:39:55 +0530

    initial

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from watchdog.observers import Observer
 import time
 import sys
 class ToWatch:
-    watchDirectory = "C:\Users\Atharva\AppData\Local\Packages\Microsoft.ScreenSketch_8wekyb3d8bbwe\TempState"#your directory, in this case its the temperory snip and sketch folder for me
+    watchDirectory = r"C:\Users\Atharva\AppData\Local\Packages\Microsoft.ScreenSketch_8wekyb3d8bbwe\TempState" #your directory, in this case its the temperory snip and sketch folder for me
 
     def __init__(self):
         self.oberver=Observer()
@@ -30,4 +30,8 @@ class Handler(FileSystemEventHandler):
         elif event.event_type == 'created':
             # Event is created, you can process it now
             print("Watchdog received created event - % s." % event.src_path)
-            
+            print (event.src_path)
+  
+if __name__ == '__main__':
+    watch = ToWatch()
+    watch.run()
